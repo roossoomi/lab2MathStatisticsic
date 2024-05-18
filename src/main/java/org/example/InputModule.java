403571a2e0e485b502e0caf819c37784692f588c
roossoomi <bazhenova.sofya@bk.ru>
2024-05-18 12:10:46 +0300

    I hope, final

--- src/main/java/org/example/InputModule.java
+++ src/main/java/org/example/InputModule.java
@@ -9,22 +9,18 @@ public class InputModule {
     public static HashMap<String, double[]> extractDataFromSheet(Sheet selectedSheet) {
         int columnCount = selectedSheet.getRow(0).getLastCellNum();
 
-        // Создаем HashMap для хранения данных по странам
-        HashMap<String, double[]> countryData = new HashMap<>();
 
-        // Проходимся по каждому столбцу, начиная со 2 элемента, и считываем данные по странам
+        HashMap<String, double[]> cellData = new HashMap<>();
+
         for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
-            // Получаем название страны из заголовка столбца
-            String countryName = selectedSheet.getRow(0).getCell(columnIndex).getStringCellValue();
 
-            // Создаем массив для хранения значений показателей
+            String cellName = selectedSheet.getRow(0).getCell(columnIndex).getStringCellValue();
+
             double[] values = new double[selectedSheet.getLastRowNum()];
 
-            // Проходимся по каждой строке, начиная со второй, и считываем значения показателей
             for (int rowIndex = 1; rowIndex <= selectedSheet.getLastRowNum(); rowIndex++) {
                 Row row = selectedSheet.getRow(rowIndex);
 
-                // Получаем значение показателя из ячейки в текущем столбце
                 Cell cell = row.getCell(columnIndex);
                 if (cell != null) {
                     switch (cell.getCellType()) {
@@ -35,18 +31,18 @@ public class InputModule {
                             values[rowIndex - 1] = cell.getNumericCellValue();
                             break;
                         default:
-                            values[rowIndex - 1] = Double.NaN; // Заполнение некорректных данных
+                            values[rowIndex - 1] = Double.NaN;
                             break;
                     }
                 } else {
-                    values[rowIndex - 1] = Double.NaN; // Заполнение пропущенных ячеек
+                    values[rowIndex - 1] = Double.NaN;
                 }
             }
 
             // Добавляем данные страны в HashMap
-            countryData.put(countryName, values);
+            cellData.put(cellName, values);
         }
 
-        return countryData;
+        return cellData;
     }
 }
